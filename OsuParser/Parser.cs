b42fa4b5e081f14c10d0c1d9c7378c623b8d157b
CloudHolic <bluegkstjr@kaist.ac.kr>
2018-01-05 12:03:15 +0900

    Delete Test Programs temporarily. Change some methods in Parser

--- OsuParser/Parser.cs
+++ OsuParser/Parser.cs
@@ -38,56 +38,86 @@ namespace OsuParser
             return new Storyboard(filename);
         }
 
+        public static string PrintOsuFile(Beatmap beatmap)
+        {
+            var encoding = new UTF8Encoding(false);
+            var mem = new MemoryStream();
+            var stream = new StreamWriter(mem, encoding);
+            SaveOsuFile(stream, beatmap);
+
+            return encoding.GetString(mem.ToArray(), 0, (int)mem.Length);
+        }
+
         public static void SaveOsuFile(string filename, Beatmap beatmap)
         {
             using (var writer = new StreamWriter(filename, false, new UTF8Encoding(false)))
             {
-                // osu file version
-                writer.WriteLine("osu file format v14");
-                writer.WriteLine();
-
-                // General section
-                GeneralParser.Writer(writer, beatmap.Gen);
-                writer.WriteLine();
-
-                // Editor section
-                EditorParser.Writer(writer, beatmap.Edit);
-                writer.WriteLine();
-
-                // Metadata section
-                MetadataParser.Writer(writer, beatmap.Meta);
-                writer.WriteLine();
-
-                // Difficulty section
-                DifficultyParser.Writer(writer, beatmap.Diff);
-                writer.WriteLine();
-
-                // Events section
-                EventParser.Writer(writer, beatmap.Events);
-                writer.WriteLine();
-
-                // TimingPoints setcion
-                TimingPointsParser.Writer(writer, beatmap.Timing);
-                writer.WriteLine();
-
-                // Colours section
-                ColorParser.Writer(writer, beatmap.Color);
-                writer.WriteLine();
-
-                // HitObjects section
-                HitObjectParser.Writer(writer, beatmap.HitObjects);
-                writer.WriteLine();
+                SaveOsuFile(writer, beatmap);
             }
+        }
+
+        public static void SaveOsuFile(StreamWriter writer, Beatmap beatmap)
+        {
+            // osu file version
+            writer.WriteLine("osu file format v14");
+            writer.WriteLine();
+
+            // General section
+            GeneralParser.Writer(writer, beatmap.Gen);
+            writer.WriteLine();
+
+            // Editor section
+            EditorParser.Writer(writer, beatmap.Edit);
+            writer.WriteLine();
+
+            // Metadata section
+            MetadataParser.Writer(writer, beatmap.Meta);
+            writer.WriteLine();
+
+            // Difficulty section
+            DifficultyParser.Writer(writer, beatmap.Diff);
+            writer.WriteLine();
+
+            // Events section
+            EventParser.Writer(writer, beatmap.Events);
+            writer.WriteLine();
+
+            // TimingPoints setcion
+            TimingPointsParser.Writer(writer, beatmap.Timing);
+            writer.WriteLine();
+
+            // Colours section
+            ColorParser.Writer(writer, beatmap.Color);
+            writer.WriteLine();
+
+            // HitObjects section
+            HitObjectParser.Writer(writer, beatmap.HitObjects);
+            writer.WriteLine();
+        }
+
+        public static string PrintOsbFile(Storyboard storyboard)
+        {
+            var encoding = new UTF8Encoding(false);
+            var mem = new MemoryStream();
+            var stream = new StreamWriter(mem, encoding);
+            SaveOsbFile(stream, storyboard);
+
+            return encoding.GetString(mem.ToArray(), 0, (int)mem.Length);
         }
 
         public static void SaveOsbFile(string filename, Storyboard storyboard)
         {
             using (var writer = new StreamWriter(filename, false, new UTF8Encoding(false)))
             {
-                // Event section only in .osb file.
-                EventParser.Writer(writer, storyboard);
-                writer.WriteLine();
+                SaveOsbFile(writer, storyboard);
             }
+        }
+
+        public static void SaveOsbFile(StreamWriter writer, Storyboard storyboard)
+        {
+            // Event section only in .osb file.
+            EventParser.Writer(writer, storyboard);
+            writer.WriteLine();
         }
     }
 }
